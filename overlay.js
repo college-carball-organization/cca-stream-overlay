fc6c0f711c4a079b8f6a58312e881dc50f99d5b4
gannonprudhomme <gannonprudhomme@gmail.com>
2018-06-30 01:02:57 -0500

    Added JSON loading to overlay

--- overlay.js
+++ overlay.js
@@ -1,26 +1,29 @@
 $(function() {
   // Get all of the data from the Editor
   // And fill in all of the forms
+  $.getJSON("http://prod.collegecarball.net/data.json", function(data) {
+    viewModel.caster1(data.caster1.name);
+    viewModel.caster2(data.caster2.name);
+    viewModel.blueName(data.blueName.name);
+    viewModel.blueP1(data.blueP1.name);
+    viewModel.blueP2(data.blueP2.name);
+    viewModel.blueP3(data.blueP3.name);
+    viewModel.blueP4(data.blueP4.name);
+    viewModel.blueP5(data.blueP5.name);
+    viewModel.orangeName(data.orangeName.name);
+    viewModel.orangeP1(data.orangeP1.name);
+    viewModel.orangeP2(data.orangeP2.name);
+    viewModel.orangeP3(data.orangeP3.name);
+    viewModel.orangeP4(data.orangeP4.name);
+    viewModel.orangeP5(data.orangeP5.name);
 
+  })
 
-  ko.applyBindings(c1ViewModel);
+  ko.applyBindings(viewModel);
 });
 
 
-var c1ViewModel = {
+var viewModel = {
   caster1: ko.observable('BDawg'),
   caster2: ko.observable('Greek Mech')
-}
-
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
 }
