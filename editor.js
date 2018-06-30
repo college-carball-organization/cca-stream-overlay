44963492980f7a8ab7d9f704ebf468752378d8ed
gannonprudhomme <gannonprudhomme@gmail.com>
2018-06-30 00:22:51 -0500

    Testing with KO observable arrays

--- editor.js
+++ editor.js
@@ -6,8 +6,7 @@ var blueP4 = $("blueP4");
 var blueP5 = $("blueP5");
 
 $(document).ready(function() {
-  $.getJSON("res/data.json", function(data) {
-
+  $.getJSON("http://prod.collegecarball.net/data.json", function(data) {
 
   })
 
@@ -19,23 +18,28 @@ $(document).ready(function() {
     })
   });
 
-  ko.applyBindings(viewModel);
+  //ko.applyBindings(viewModel);
 });
 
-var viewModel = {
-  // Default values for the fields?
-  caster1: ko.observable("Caster 1"),
-  caster2: ko.observable("Caster 2"),
-  blueName: ko.observable("Blue Team Name"),
-  blueP1: ko.observable("Blue Player 1"),
-  blueP2: ko.observable("Blue Player 2"),
-  blueP3: ko.observable("Blue Player 3"),
-  blueP4: ko.observable("Blue Player 4"),
-  blueP5: ko.observable("Blue Player 5"),
-  orangeName: ko.observable("Orange Team Name"),
-  orangeP1: ko.observable("Orange Player 1"),
-  orangeP2: ko.observable("Orange Player 2"),
-  orangeP3: ko.observable("Orange Player 3"),
-  orangeP4: ko.observable("Orange Player 4"),
-  orangeP5: ko.observable("Orange Player 5"),
-}
+var observableArray = ko.observableArray([
+  {caster1: "Bdawg"},
+  {caster2: "Xotic"}
+])
+
+// var viewModel = {
+//   // Default values for the fields?
+//   caster1: ko.observable("Caster 1"),
+//   caster2: ko.observable("Caster 2"),
+//   blueName: ko.observable("Blue Team Name"),
+//   blueP1: ko.observable("Blue Player 1"),
+//   blueP2: ko.observable("Blue Player 2"),
+//   blueP3: ko.observable("Blue Player 3"),
+//   blueP4: ko.observable("Blue Player 4"),
+//   blueP5: ko.observable("Blue Player 5"),
+//   orangeName: ko.observable("Orange Team Name"),
+//   orangeP1: ko.observable("Orange Player 1"),
+//   orangeP2: ko.observable("Orange Player 2"),
+//   orangeP3: ko.observable("Orange Player 3"),
+//   orangeP4: ko.observable("Orange Player 4"),
+//   orangeP5: ko.observable("Orange Player 5"),
+// }
