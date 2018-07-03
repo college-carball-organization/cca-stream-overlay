8f0f2acbfb1c1a204ee594314ed980dd869a5a40
gannonprudhomme <gannonprudhomme@gmail.com>
2018-07-03 11:44:00 -0500

    PHP Syntax errors

--- save_json2.php
+++ save_json2.php
@@ -22,18 +22,18 @@
 
     $myObj->caster1 = $_POST['caster1'];
     $myObj->caster2 = $_POST['caster2'];
-    $myObj->blueName = $_POST['blueName'],
-    $myObj->orangeName = $_POST['orangeName'],
-    $myObj->blueP1 = $_POST['blueP1'],
-    $myObj->blueP2 = $_POST['blueP2'],
-    $myObj->blueP3 = $_POST['blueP3'],
-    $myObj->blueP4 = $_POST['blueP4'],
-    $myObj->blueP5 = $_POST['blueP5'],
-    $myObj->orangeP1 = $_POST['orangeP1'],
-    $myObj->orangeP2 = $_POST['orangeP2'],
-    $myObj->orangeP3 = $_POST['orangeP3'],
-    $myObj->orangeP4 = $_POST['orangeP4'],
-    $myObj->orangeP5 = $_POST['orangeP5'],
+    $myObj->blueName = $_POST['blueName'];
+    $myObj->orangeName = $_POST['orangeName'];
+    $myObj->blueP1 = $_POST['blueP1'];
+    $myObj->blueP2 = $_POST['blueP2'];
+    $myObj->blueP3 = $_POST['blueP3'];
+    $myObj->blueP4 = $_POST['blueP4'];
+    $myObj->blueP5 = $_POST['blueP5'];
+    $myObj->orangeP1 = $_POST['orangeP1'];
+    $myObj->orangeP2 = $_POST['orangeP2'];
+    $myObj->orangeP3 = $_POST['orangeP3'];
+    $myObj->orangeP4 = $_POST['orangeP4'];
+    $myObj->orangeP5 = $_POST['orangeP5'];
 
     $myJSON = json_encode($myObj);
     echo $myJSON;
