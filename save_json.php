743dcd7319ebd0805928f88a6447ae7928d05f74
gannonprudhomme <gannonprudhomme@gmail.com>
2018-07-02 21:11:28 -0500

    Added PHP error logging

--- save_json.php
+++ save_json.php
@@ -4,4 +4,7 @@ $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = $_GET["data"];
 fwrite($fh, $stringData);
 fclose($fh)
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+error_log( "Hello, errors!" );
 ?>
