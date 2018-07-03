f7b4ccc562012c6f94a35f8b0bc4656862744b53
gannonprudhomme <gannonprudhomme@gmail.com>
2018-07-03 11:41:51 -0500

    Changed JSON saving from arrays to PHP objects

--- save_json2.php
+++ save_json2.php
@@ -1,9 +1,9 @@
 <?php
   $myFile = "data.json";
-  $arr_data = array();
+  //$arr_data = array();
 
   try {
-    $formdata = array(
+    $formdata = [
       'caster1'=> $_POST['caster1'],
       'caster2'=> $_POST['caster2'],
       'blueName'=> $_POST['blueName'],
@@ -18,7 +18,25 @@
       'orangeP3'=> $_POST['orangeP3'],
       'orangeP4'=> $_POST['orangeP4'],
       'orangeP5'=> $_POST['orangeP5'],
-    );
+    ];
+
+    $myObj->caster1 = $_POST['caster1'];
+    $myObj->caster2 = $_POST['caster2'];
+    $myObj->blueName = $_POST['blueName'],
+    $myObj->orangeName = $_POST['orangeName'],
+    $myObj->blueP1 = $_POST['blueP1'],
+    $myObj->blueP2 = $_POST['blueP2'],
+    $myObj->blueP3 = $_POST['blueP3'],
+    $myObj->blueP4 = $_POST['blueP4'],
+    $myObj->blueP5 = $_POST['blueP5'],
+    $myObj->orangeP1 = $_POST['orangeP1'],
+    $myObj->orangeP2 = $_POST['orangeP2'],
+    $myObj->orangeP3 = $_POST['orangeP3'],
+    $myObj->orangeP4 = $_POST['orangeP4'],
+    $myObj->orangeP5 = $_POST['orangeP5'],
+
+    $myJSON = json_encode($myObj);
+    echo $myJSON;
 
     // Get data from existing json file
     //$jsondata = file_get_contents($myFile);
@@ -26,16 +44,16 @@
     // converts json data into array
     //$arr_data = json_decode($jsondata, true);
 
-    $arr_data = [];
+    //$arr_data = [];
 
     // Push user data to array
-    array_push($arr_data, $formdata);
+    //array_push($arr_data, $formdata);
 
     // Convert updated array to json
-    $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
+    //$jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
 
     // Write json data into data.json file
-    if(file_put_contents($myFile, $jsondata)) {
+    if(file_put_contents($myFile, $myJSON)) {
       echo 'Data sucessfully saved';
     } else {
       echo 'Error';
