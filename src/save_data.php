eb96bf06a5af1245fef434d47301460c1152d4f2
Michael Doyle <mcdoyle7227@gmail.com>
2020-10-01 22:14:36 -0400

    Make docker volume writable to allow data.json writes

--- src/save_data.php
+++ src/save_data.php
@@ -54,7 +54,7 @@
       header("Location: https://prod.cca.gg/control-panel/index.html");
       exit();
     } else {
-      //echo 'Error';
+      echo 'Error';
     }
   } catch(Exception $e) {
     echo 'Caught Exception: ', $e->getMessage(), '\n';
